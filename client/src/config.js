019f15e48fcd74f01832892c2414cfeb226045ac
Hamza Malik <muhammadhamzamlk#gmail.com>
2023-11-25 17:11:37 +0500

    URL Changed

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
-// const USER_API_URL = 'https://main-blogs-web-app.onrender.com/api/users';
-// const POST_API_URL = 'https://main-blogs-web-app.onrender.com/api/post';
-const USER_API_URL = 'http://localhost:4050/api/users';
-const POST_API_URL = 'http://localhost:4050/api/post';
+const USER_API_URL = 'https://main-blogs-web-app.onrender.com/api/users';
+const POST_API_URL = 'https://main-blogs-web-app.onrender.com/api/post';
+// const USER_API_URL = 'http://localhost:4050/api/users';
+// const POST_API_URL = 'http://localhost:4050/api/post';
 
 // export it
 export { USER_API_URL, POST_API_URL }
