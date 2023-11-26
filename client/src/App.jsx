6fa4130f9d9a571e556577bc7c011d8b75947ff5
Hamza Malik <muhammadhamzamlk#gmail.com>
2023-11-26 23:49:37 +0500

    url checker added in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,21 +10,50 @@ import { Login } from './Components/Auth/Login';
 import Blogs from './Components/Blogs/Blogs';
 import Contact from './Components/Contact';
 import Users from './Components/Profile/Users';
-import { Toaster } from 'react-hot-toast';
+import { Toaster, toast } from 'react-hot-toast';
 import Editor from './Components/Editor/Editor';
 import AddPost from './Components/Blogs/AddPost';
 import Posts from './Components/Blogs/Posts';
 import PostDetail from './Components/Blogs/PostDetail';
 import EditPost from './Components/Blogs/EditPost';
 import URLChecker from './Components/Practices/URLChecker';
+import { CHECK_URL } from './config';
+import axios from 'axios';
 
 function App() {
   const location = useLocation();
   const isAuthPage = location.pathname === '/signup' || location.pathname === '/login';
   const user = localStorage.getItem('user');
+
+  useEffect(() => {
+    const SERVER_URL = 'https://main-blogs-web-app.onrender.com/api/post';
+
+    const checkUrlValidity = async () => {
+      try {
+        const response = await axios.get(`${CHECK_URL}/?url=https://main-blogs-web-app.onrender.com/api/post`);
+        const { isValid } = response.data;
+
+        // Assuming `settings` is defined somewhere in your component
+        toast.promise(
+          isValid
+            ? Promise.resolve()
+            : Promise.reject(new Error('Invalid URL')),
+          {
+            loading: 'Checking URL...',
+            success: <span>Hurry! Backend is Connected!</span>,
+            error: <span>Server Error.</span>,
+          }
+        );
+      } catch (error) {
+        toast.error('Server Error.');
+      }
+    };
+
+    checkUrlValidity();
+  }, []); // Empty dependency array ensures the effect runs only once
+
   return (
     <>
-
       {!isAuthPage && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
@@ -44,9 +73,7 @@ function App() {
         <Route path="/post/add" element={user ? <AddPost /> : <Navigate to={'/login'} />} />
       </Routes>
       {!isAuthPage && <Footer />}
-      <Toaster
-        position="top-center"
-      />
+      <Toaster position="top-center" />
     </>
   );
 }
