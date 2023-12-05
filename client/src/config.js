198a847986e53ce0be7683841a3b4acda92206e7
Muhammad Hamza Malik <99534215+hamzadevlpr@users.noreply.github.com>
2023-12-05 18:55:26 +0500

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,11 +1,11 @@
-// const USER_API_URL = 'https://main-blogs-web-app.onrender.com/api/users';
-// const POST_API_URL = 'https://main-blogs-web-app.onrender.com/api/post';
-// const COMMENTS_API_URL = 'https://main-blogs-web-app.onrender.com/api/comments';
-// const CHECK_URL = 'https://main-blogs-web-app.onrender.com/check-url';
-const USER_API_URL = 'http://localhost:4050/api/users';
-const POST_API_URL = 'http://localhost:4050/api/post';
-const COMMENTS_API_URL = 'http://localhost:4050/api/post/comments';
-const CHECK_URL = 'http://localhost:4050/check-url';
+const USER_API_URL = 'https://main-blogs-web-app.onrender.com/api/users';
+const POST_API_URL = 'https://main-blogs-web-app.onrender.com/api/post';
+const COMMENTS_API_URL = 'https://main-blogs-web-app.onrender.com/api/comments';
+const CHECK_URL = 'https://main-blogs-web-app.onrender.com/check-url';
+// const USER_API_URL = 'http://localhost:4050/api/users';
+//const POST_API_URL = 'http://localhost:4050/api/post';
+//const COMMENTS_API_URL = 'http://localhost:4050/api/post/comments';
+//const CHECK_URL = 'http://localhost:4050/check-url';
 
 // export it
 export { USER_API_URL, POST_API_URL, COMMENTS_API_URL, CHECK_URL }
